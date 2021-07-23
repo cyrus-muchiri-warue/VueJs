9d9d69d295472c20e453acbfa9461ca5c59ebda8
cyrus-muchiri-warue <cyrus5078@gmail.com>
2021-07-23 11:52:43 +0300

    basic method and data manupulation

--- js/styles.js
+++ js/styles.js
@@ -3,10 +3,17 @@ $var = new Vue(
         el: "#wrapper",
         data: {
           first:'cyrus',  
-          last:'muchiri',  
+            last: 'muchiri',
+            age:20
         },
         methods: {
-
+            addAge: function (params) {
+                return this.age +params
+               
+            },
+            subtractAge:function(params) {
+               return this.age + params;
+            }
         },
         computed: {
             
